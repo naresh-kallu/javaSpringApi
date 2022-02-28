bc2159b0fa883311a530ecc7c68fc382e005370d
tectoro <tectoro@tectoro-OptiPlex-5060>
2022-02-28 14:41:19 +0530

    success

---
+++ java-code-core/src/main/java/com/tectoro/JavaCodeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.tectoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeCoreApplication.class, args);
+	}
+
+}
---
+++ java-code-core/src/test/java/com/tectoro/JavaCodeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tectoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
